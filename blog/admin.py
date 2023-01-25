5ad3731a3b99ae35a769d687fca462bd4157be20
pincoin <pincoins@gmail.com>
2023-01-26 01:50:12 +0900

    book and blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from django.contrib.admin.filters import SimpleListFilter
+from django.utils.translation import gettext_lazy as _
+from mptt.admin import DraggableMPTTAdmin
 
-# Register your models here.
+from blog import models
+
+
+class PostNullFilterSpec(SimpleListFilter):
+    title = _('post')
+    parameter_name = 'post'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', _('Has post'),),
+            ('0', _('No post'),),
+        )
+
+    def queryset(self, request, queryset):
+        kwargs = {
+            '{}'.format(self.parameter_name): None,
+        }
+        if self.value() == '0':
+            return queryset.filter(**kwargs)
+        if self.value() == '1':
+            return queryset.exclude(**kwargs)
+        return queryset
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'chunk_size', 'block_size')
+    ordering = ['-created']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blog', 'slug', 'owner', 'category', 'published', 'status', 'is_removed', 'ip_address')
+    list_filter = ('status', 'published')
+    list_select_related = ('blog', 'category', 'owner')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published'
+    ordering = ['status', '-published']
+    raw_id_fields = ('owner',)
+    readonly_fields = ('ip_address', 'view_count')
+
+
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'indented_title', 'slug', 'blog')
+    list_filter = ('blog__title', 'created')
+    prepopulated_fields = {'slug': ('title',)}
+    mptt_level_indent = 20
+    ordering = ['tree_id', 'lft']
+
+
+class TaggedItemInline(admin.StackedInline):
+    model = models.TaggedPost
+
+
+class PostTagAdmin(admin.ModelAdmin):
+    inlines = [TaggedItemInline]
+    list_display = ['name', 'slug']
+    ordering = ['name', 'slug']
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'file', 'created')
+    list_filter = (PostNullFilterSpec,)
+    ordering = ['-created', ]
+
+
+admin.site.register(models.Blog, BlogAdmin)
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.PostTag, PostTagAdmin)
+admin.site.register(models.Attachment, AttachmentAdmin)
--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.admin.filters import SimpleListFilter
+from django.utils.translation import gettext_lazy as _
+from mptt.admin import (
+    DraggableMPTTAdmin, MPTTModelAdmin
+)
 
-# Register your models here.
+from book import models
+
+
+class PageNullFilterSpec(SimpleListFilter):
+    title = _('page')
+    parameter_name = 'page'
+
+    def queryset(self, request, queryset):
+        kwargs = {
+            '{}'.format(self.parameter_name): None,
+        }
+        if self.value() == '0':
+            return queryset.filter(**kwargs)
+        if self.value() == '1':
+            return queryset.exclude(**kwargs)
+        return queryset
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'owner', 'status', 'view_count')
+    ordering = ['-created']
+    raw_id_fields = ('owner',)
+    readonly_fields = ('view_count', 'updated')
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'page', 'file', 'created')
+    list_filter = (PageNullFilterSpec,)
+    ordering = ['-created', ]
+
+
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'indented_title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    mptt_level_indent = 20
+    ordering = ['tree_id', 'lft']
+
+
+class PageAdmin(MPTTModelAdmin):
+    list_display = ('title',)
+    list_filter = ('status', 'book')
+    mptt_level_indent = 20
+    readonly_fields = ('ip_address', 'view_count')
+    ordering = ['tree_id', 'lft']
+
+
+admin.site.register(models.Book, BookAdmin)
+admin.site.register(models.Attachment, AttachmentAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Page, PageAdmin)
