b6cbcb07c0191dc80ca350e1ca5238c847bbfd9f
pincoin <pincoins@gmail.com>
2023-02-06 22:25:12 +0900

    redirect admin page

--- conf/views.py
+++ conf/views.py
@@ -1,5 +1,6 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 
-class HomeView(generic.TemplateView):
-    template_name = 'conf/home.html'
+class HomeView(generic.RedirectView):
+    url = reverse_lazy('admin:index')
